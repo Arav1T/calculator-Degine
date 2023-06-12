71e8ccfd701224df17561b05fd97271c4e7d2d38
Arav1T <aravtiwari127@gmail.com>
2023-06-12 17:56:54 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      height:{
+         "130":"32.5rem",
+         "150":"49.5rem"
+      },
+      width:{
+        "90":"23rem"
+      }
+    },
+  },
+  plugins: [],
+}
